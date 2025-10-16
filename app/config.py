072d46c9441f4f89526047ceed156ea864701483
usuarioap03-hash <usuarioap03@gmail.com>
2025-10-16 14:43:42 -0500

    prueba de geozona

--- app/config.py
+++ app/config.py
@@ -3,8 +3,12 @@
 import os
 import pytz
 
-PLANTA_LAT = 9.25831;
-PLANTA_LON = -79.65848;
+# PLANTA_LAT = 9.25831;
+# PLANTA_LON = -79.65848;
+# RANGO_METROS = 300;
+
+PLANTA_LAT = 9.028398007355186;
+PLANTA_LON = -79.46020053317156;
 RANGO_METROS = 300;
 
 # ⚙️ Configuración general para la aplicación en Render
